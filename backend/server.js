535e1d21148c808fbeace3b11f969a53bf1443f7
Faisal Al Qasimi <falqas@gmail.com>
2018-05-09 16:10:00 -0700

    add /blogposts get & post routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const BlogPost = require('./models/blogpost');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
+
+app.get('/blogposts', function (req, res) {
+  console.log('get works!')
+});
+
+app.post('/blogposts', function (req, res) {
+  console.log('post works!')
+  console.log(req.body)
+});
+
 
 app.listen(3000);
