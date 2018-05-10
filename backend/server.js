e9c09b1c41574aa340d7a1848eae317c7f4ca31d
Faisal Al Qasimi <falqas@gmail.com>
2018-05-09 21:06:56 -0700

    add db controllers for index & create

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,21 @@ app.get('/', function (req, res) {
 });
 
 app.get('/blogposts', function (req, res) {
-  console.log('get works!')
+    console.log('received a GET request!');
+    console.log('request body is', req.body);
+    BlogPost.find({}, function (err, blogPosts) {
+        if (err) res.send(err);
+        else res.json(blogPosts);
+    });
 });
 
 app.post('/blogposts', function (req, res) {
-  console.log('post works!')
-  console.log(req.body)
+    console.log('received a POST request!');
+    console.log('request body is', req.body);
+    BlogPost.create(req.body, function (err, blogpost) {
+        if (err) res.send(err);
+        else res.json(blogpost);
+    });
 });
-
 
 app.listen(3000);
